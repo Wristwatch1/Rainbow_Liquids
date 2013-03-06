e56f3fcf1cc98c9c871c666644e2ec65ad94d7fa
Wristwatch1 <daxadventures@xtra.co.nz>
2013-03-06 14:20:40 +1300

    Liquids almost finished

--- rainbow/common/BlockWhiteSpectraStill.java
+++ rainbow/common/BlockWhiteSpectraStill.java
@@ -1,9 +1,14 @@
 package wristwatch1.rainbow.common;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockStationary;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -18,7 +23,20 @@ public class BlockWhiteSpectraStill extends BlockStationary
 		this.setLightOpacity(3);
 		this.disableStats();
 		this.setRequiresSelfNotify();
-	}
+		}
+		
+	public void onEntityCollidedWithBlock(World world, int i, int j, int k, Entity entity)
+	{
+		if (entity instanceof EntityLiving) 
+		{         
+			((EntityLiving) entity).addPotionEffect(new PotionEffect(Potion.heal.getId(),2 * 20, 0));
+			}  
+		{
+		
+		}
+		
+		}
+
        public String getTextureFile()
 	  {
 	          return "/rainbowwater.png";
